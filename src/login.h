1e6020774248213bf316427b64580fed152bad5a
Baldomero Vargas <bvarg006@hammer.cs.ucr.edu>
2016-02-06 13:37:27 -0800

    Added comments and skeleton for entire shell

--- src/login.h
+++ src/login.h
@@ -2,18 +2,23 @@
 #define __login_h__
 #include <unistd.h>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+/* prints command prompt*/
+/*gets user name and hostname*/
 void command_prompt(){
 	char * name = getlogin();
 	if(name == NULL){
 		cout << "Error: Cannot retreieve login info." << endl;
+		exit(EXIT_FAILURE);
 	}	
 	char hostname[256]; 
 	int hostcheck =  gethostname(hostname, 255);
 	if(hostcheck == -1){
 		cout << "Error: Cannot retrieve login info." << endl;
+		exit(EXIT_FAILURE);
 	}
 	cout << name << "@" << hostname << "$ ";
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 int main(){
-	command_prompt();	
+	/*currently set at 1 for 1 itteration to avoid infinite loop*/
+	bool exit_check = 1;
 
+	/*entire itteration of rshell until exit is called*/
+	/*need to make exit function*/
+	do{
+		/*prints command prompt every itteration*/
+		command_prompt();	
+
+	}while(!exit_check);
 
 	return 0;
 }
