240467f8663c045b2543fcd25d84e3e397578301
Samuel Hwang <shwan022@hammer.cs.ucr.edu>
2016-02-07 21:55:03 -0800

    Converted string to cstring after parsing

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include<stdio.h>
 #include<cstring>
 #include"login.h"
-#include "Tokenizer.h"
+#include"Tokenizer.h"
 
 using namespace std;
 
@@ -51,32 +51,20 @@ int main(int argc, char * argv[]){
 		}
 	}while(exit_check == 0);
 
-	/*Tokenizer str;
+	Tokenizer str;
 	string token;
 	string line;
 
-	cout << "Sample of parsing between spaces please enter command(s)" << endl;
 	getline(cin, line);
-	str.set(line); //default delimiter to separate string is " "
+	str.set(line, ";"); //extra parameter changes delimiter to all characters in second string
 
 	while((token = str.next()) != "")
 	{
 		cout << token << endl;
 	}
 	cout << endl;
-
-	cout << "Sample of parsing with different delimiters. Separate words with $" << endl;
-	getline(cin, line);
-	str.set(line, "$"); //extra parameter changes delimiter to all characters in second string
-
-	while((token = str.next()) != "")
-	{
-		cout << token << endl;
-	}
-	cout << endl;
-
-	cout << "So if I want to separate with the connectors according to the assignment the change should be easy. Type \"ls -a; echo hello; mkdir test\"" << endl;
-	getline(cin, line);
+	
+	/*getline(cin, line);
 	string delimiter = "; ";
 	
 	size_t pos = 0;
@@ -86,20 +74,7 @@ int main(int argc, char * argv[]){
 		cout << token << endl;
 		line.erase(0, pos + delimiter.length());
 	}
-	cout << line << endl;
-	//this works but
-	//i think this is the wrong way to do it. Ill look up a way to use str.set 
-
-
-	//this option looks at the indivudual characters instead of the whole line
-	//str.set(line, delimiter);
-	//
-
-	//while((token = str.next()) != "")
-	//{
-	//	cout << token << endl;
-	//}
-	//cout << endl;*/
+	cout << line << endl;*/
 
 
 	return 0;
--- src/token_test.cpp
+++ src/token_test.cpp
@@ -2,31 +2,46 @@
 #include "Tokenizer.h"
 #include <string>
 #include <iostream>
+#include <vector>
+#include<cstring>
 using std::string;
 using std::cout;
 using std::endl;
+using namespace std;
 
 int main(int argc, char* argv[])
 {
     // instanciate Tokenizer class
     Tokenizer str;
     string token;
-    int counter = 0;
-	string test = "This string tests parsing with variables";
+	vector<string> holdCommands;
+	string test = "ls -a; echo hello; cd ..";
     // set source string with default delimiters (space, tab, and newline char)
-    str.set(test);
+    str.set(test, ";");
 
     // Tokenizer::next() returns a next available token from source string
     // If it reaches EOS, it returns zero-length string, "".
     while((token = str.next()) != "")
     {
-        ++counter;
-        cout << counter << ": " << token << endl;
+        holdCommands.push_back(token);
     }
-    cout << endl;
+    for(int i = 0; i < holdCommands.size(); i++)
+	{
+		cout << holdCommands.at(i) << endl;
+	}
+
+	for(int i = 0; i < holdCommands.size(); i++)
+	{
+		char *y = new char[holdCommands.at(i).length() + 1];
+		strcpy(y, holdCommands.at(i).c_str());
+
+		cout << y << endl;
+	}
+	
+	
 
 
-    // set a different string and delimiters(~, _)
+   /* // set a different string and delimiters(~, _)
     // Notice that space is not delimiter any more.
     counter = 0;
     str.set("Now,~the_delimiters~~~are__changed~to_tild  and  underscore.~", "~_");
@@ -36,6 +51,6 @@ int main(int argc, char* argv[])
         cout << counter << ": " << token << endl;
     }
 
-    return 0;
+    return 0;*/
 }
 
