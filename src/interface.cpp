6f9cdf35de731cb62895138bb2a79e550ff79df3
Baldomero Vargas <bvarg006@hammer.cs.ucr.edu>
2016-02-10 02:25:27 -0800

    Cleared main for parsing adding, working on && in interface.cpp

--- src/interface.cpp
+++ src/interface.cpp
@@ -23,12 +23,12 @@ int main()
 
 	//all testing until parsing is finshied
 	vector<string> connectors;
-	connectors.push_back(";");
+	connectors.push_back("&&");
 	connectors.push_back(";");
 	vector<vector<string> > commands;
 	//first command
 	vector<string> test;
-	test.push_back("echo"); 
+	test.push_back("ecxho"); 
 	test.push_back("FISRT"); 
 	commands.push_back(test);
 	//second command
@@ -36,8 +36,7 @@ int main()
 	test2.push_back("cal"); 
 	commands.push_back(test2);
 	vector<string> test3;
-	test3.push_back("ls"); 
-	test3.push_back("-l"); 
+	test3.push_back("date"); 
 	commands.push_back(test3);
 	
 	
@@ -53,10 +52,15 @@ int main()
 		//fills with NULL
 		char * buffer[1024];
 		memset(buffer, '\0', sizeof(buffer));
-		//string curr_connect = connectors.at(it);
+		if(it != 0)
+		{
+			prev_command = connectors.at(it - 1);
+			cout << prev_command << " ";
+		}
 		//for(unsigned i = 0; i < commands.size(); ++i)
 		//{
 			//check if previous call failed or not
+			cout << prev_check << endl;
 			if(prev_check)
 			{
 				if(prev_command == ";")
@@ -84,11 +88,14 @@ int main()
 					}
 					else
 					{
+						prev_check = true;
+						//in child process
 						int run_shell = execvp(buffer[0],buffer);
 						if(run_shell < 0)
 						{
 							perror("Execvp failed");
 							prev_check = false;
+							cout << "made it" << endl;
 						}
 
 
@@ -99,6 +106,39 @@ int main()
 				else if(prev_command == "&&")
 				{
 					//will run
+
+					//for loop to make command into a cstring array
+					for(int cs = 0; cs < commands.at(it).size() ; ++cs)
+					{
+						buffer[cs] = const_cast<char *>(commands.at(it).at(cs).c_str());
+					}
+
+					//run execvp
+					pid_t childPID = fork();
+					if(childPID < 0)
+					{
+						//forking error
+						perror("Forking child Failed\n");
+						exit(-1);
+					}
+					else if(childPID != 0)
+					{
+						//parent process
+						wait(NULL);	
+					}
+					else
+					{
+						prev_check = true;
+						//in child process
+						int run_shell = execvp(buffer[0],buffer);
+						if(run_shell < 0)
+						{
+							perror("Execvp failed");
+							prev_check = false;
+						}
+
+
+					}
 
 				}
 				else if(prev_command == "||")
@@ -137,6 +177,7 @@ int main()
 					}
 					else
 					{
+						prev_check = true;
 						int run_shell = execvp(buffer[0],buffer);
 						if(run_shell < 0)
 						{
@@ -153,6 +194,42 @@ int main()
 				else if(prev_command == "&&")
 				{
 					//will not run
+
+			/*		//for loop to make command into a cstring array
+					for(int cs = 0; cs < commands.at(it).size() ; ++cs)
+					{
+						buffer[cs] = const_cast<char *>(commands.at(it).at(cs).c_str());
+					}
+
+					//run execvp
+					pid_t childPID = fork();
+					if(childPID < 0)
+					{
+						//forking error
+						perror("Forking child Failed\n");
+						exit(-1);
+					}
+					else if(childPID != 0)
+					{
+						//parent process
+						wait(NULL);	
+					}
+					else
+					{
+						//in child process
+						int run_shell = execvp(buffer[0],buffer);
+						if(run_shell < 0)
+						{
+							perror("Execvp failed");
+							prev_check = false;
+						}
+
+
+					}
+
+					*/
+
+					prev_check = false;
 
 				}
 				else if(prev_command == "||")
@@ -176,42 +253,3 @@ int main()
 
 //}
 
-
-/*void and_connect(bool prev_check){
-//runs and logic
-////if true run next
-////if false dont
-
-
-}
-
-
-
-
-void or_connect(bool prev_check){
-//runs or logic
-//if false run next
-//if true skip
-
-
-
-}
-
-
-
-void semi_connect(bool prev_check){
-//always run command fail or not
-
-
-
-
-}
-
-
-
-void comment(){
-
-
-
-
-}*/
--- src/main.cpp
+++ src/main.cpp
@@ -15,93 +15,20 @@ using namespace boost;
 
 int main(int argc, char * argv[])
 {
-	/*currently set at 1 for 1 itteration to avoid infinite loop*/
-	int exit_check = 0;
+	int exit_check = 1;
 
 	/*entire itteration of rshell until exit is called*/
-do{
-		//initializes parsing
-		
-	vector<string> holdCommands;
-	string str;
-	command_prompt();
-	getline(cin,str);
-	typedef tokenizer<char_separator<char> > tokenizer;
-	char_separator<char> sep(" ");
-	tokenizer tokens(str, sep);
-	for(tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); tok_iter++)
-	{
-		holdCommands.push_back(*tok_iter);
-		string test;
-		test = *tok_iter;
-	}
+	do{
+		//outputs the command prompt with user login info and hostname
+		string cmd_line;
+		command_prompt();
+		getline(cin,cmd_line);
 
-	//new added
-	//for(unsigned i = 1; i < holdCommands.size(); ++i){
-	//	holdCommands.at(i).erase(0,1);
-	//}
+		//INSERT PARSING HERE
 
-	char *  cmd_line[2048];  
-	memset(cmd_line,'\0',sizeof(cmd_line));
-	for(unsigned i = 0; i < holdCommands.size(); i++)
-	{	
-		cmd_line[i] = const_cast<char*>( holdCommands.at(i).c_str());
-		cout << cmd_line[i] << endl;
-	}
-
-		//testin execvp and fork
-		pid_t childPID = fork();
-		if(childPID <  0){
-			//forking error
-			perror("Forking Failed\n");
-			exit(-1);
-		}
-		else if(childPID != 0){
-			//parent process lies here
-			wait(NULL); //waits for child to finish
-		}
-		else{
-			//child process lies here
-			//exit is not working still
-			//if(strcmp(y , "exit") == 0){
-			//	exit_check = 1;
-			//	exit(0);
-		//	}
-			//else{
-				int run_shell = execvp(cmd_line[0],cmd_line );
-				if(run_shell < 0){
-					perror("Exec Failed");
-					exit(-1);
-				}
-			//}
-		}
 	
 	}while(exit_check == 0);
 
-	/*Tokenizer str;
-	string token;
-	string line;
-
-	getline(cin, line);
-	str.set(line, ";"); //extra parameter changes delimiter to all characters in second string
-
-	while((token = str.next()) != "")
-	{
-		cout << token << endl;
-	}
-	cout << endl;*/
-	
-	/*getline(cin, line);
-	string delimiter = "; ";
-	
-	size_t pos = 0;
-	while((pos = line.find(delimiter)) != string::npos)
-	{
-		token = line.substr(0, pos);
-		cout << token << endl;
-		line.erase(0, pos + delimiter.length());
-	}
-	cout << line << endl;*/
 
 	return 0;
 }
