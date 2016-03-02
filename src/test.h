90f184b848bc39d53af1ee51989b76d3eb040264
Baldomero Vargas <bvarg006@hammer.cs.ucr.edu>
2016-03-02 15:40:45 -0800

    Further improving test funciton

--- src/test.h
+++ src/test.h
@@ -10,6 +10,8 @@ void test_execution(char * command[1024], bool & e_check)
 {
 	struct stat buf;
 	string flag;
+	string t_rue = "(True)\n";
+	string f_alse = "(False)\n";
 	//test to see if 2nd parameter is a correct flag
 	//test to see if no flag is passed
 	if(command[1] != "-e" || command[1] != "-d" ||command[1] != "-f" ||)
@@ -27,6 +29,23 @@ void test_execution(char * command[1024], bool & e_check)
 	}
 
 	//implementation of test command
+	
+	//checks if the file/directory exists
+	if(command[1] == "-e")
+	{
+		flag = "-e";
 
+	}
+	//checks if the file/directory exists and is a file
+	else if(command[1] == "-f")
+	{
+		flag = "-f";
+
+	}
+	//checks if the file/directory exists and is a directory
+	else
+	{
+		flag = "-d";
+	}
 }
 #endif
